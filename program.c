70e4a1b51aeb50fbd8ecc779b3dd9750c680d840
Darren Frempong <dafremp@gmail.com>
2026-03-17 10:04:23 +0000

    Update program.c

--- program.c
+++ program.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
-int main()
+int average(int a,int b);
+int num1, num2
+int main(){
+
+int average(int a, int b){
+printf("%d × %d", a,b);
+}
+
+printf("Enter your first number");
+scanf("%d",& num1);
+
+printf("");
+printf("Enter your second number");
+scanf("%d",&num2);
+
+return 0;
+}
